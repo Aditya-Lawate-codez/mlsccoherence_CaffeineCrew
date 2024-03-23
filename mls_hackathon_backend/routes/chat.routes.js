0477d623125f5353643916a5b6ab17fdca96b703
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-03-23 18:03:55 +0530

    user llogin register created

--- mls_hackathon_backend/routes/chat.routes.js
+++ mls_hackathon_backend/routes/chat.routes.js
@@ -3,11 +3,14 @@ const router=express.Router();
 const getConversationBySenderId=require('../controller/getConversationBySenderId')
 const saveConversation=require('../controller/saveConversation'); 
 const saveUser = require('../controller/saveUser');
+const { createUser, getUser } = require('../controller/create/user');
 
 
 router.post('/create',saveConversation)
 router.get('/:senderId',getConversationBySenderId)
 router.post('/save',saveUser)
+router.post('/register',createUser)
+router.post('/login',getUser)
 
 
 module.exports=router
